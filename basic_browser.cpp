2e289ad02002f91eff891f70a3ea477406fc0861
ammarozkan <61326832+ammarozkan@users.noreply.github.com>
2022-10-21 23:02:29 +0300

    VeepreWeb0.1 Update
    With that update, you can send parameters to web server and get amaaazing pages, thanks to them.

--- basic_browser.cpp
+++ basic_browser.cpp
@@ -5,49 +5,72 @@
 
 #define PROT_OPT(v1, v2, d1, d2, d3) {(char)v1, (char)v2, (char)d1, (char)d2, (char)d3}
 
+void sendInt(ISocketCl *client, int val)
+{
+	char *cval = (char*)calloc(sizeof(char),4); cval = (char*)&val;
+	client->Send(cval, 4);
+}
+
+void sendChar(ISocketCl *client, char val)
+{
+	client->Send(&val,1);
+}
+
 int main(int argc, char *argv[])
 {
 	char ip[16]; // Answer from DNS Server
 	int dir[3] = {0,0,0};
-	std::string dns = "veepreweb";
+	std::string servername = "veepreweb";
 	if(argc>4) for(int i = 0;i<3;i++) dir[i] = atoi(argv[i+2]);
 	{
 		ISocketCl client(SOCK_STREAM);
-		client.Connect("192.168.88.128",8000); //Connecting to DNS with 8000 port
+		client.Connect("192.168.88.128",8000); //Connecting to Name Server with 8000 port
 						       //
-		if(argc>1) dns = argv[1]; // if entered a domain, program will search entered domain (like basic_browser sitedomain)
+		if(argc>1) servername = argv[1]; // if entered a server name, program will search entered name (like basic_browser sitedomain)
 					  // else program will search veepreweb
 
-		client.Send((char*)dns.c_str(),dns.size()); // Client will send the domain
+		client.Send((char*)servername.c_str(),servername.size()); // Client will send the domain
 		
 		client.Read(ip, 16); // reading answer
 		
 		client.Close();
 	}
 	
-	std::cout << "Answer From DNS Server:" << ip << std::endl;
+	std::cout << "Answer From Name Server:" << ip << std::endl;
 	if(std::string(ip)!="") // if IP is filled with data
 	{
-		std::cout << "Trying to reach website (" << dns << ":" << ip << ")." << std::endl;
+		std::cout << "Trying to reach website (" << servername << ":" << ip << ")." << std::endl;
 		ISocketCl websiteClient(SOCK_STREAM);
 		websiteClient.Connect(ip,4545); // Connecting website with port 4545
 
-		char optV[5] = PROT_OPT(0, 0, dir[0], dir[1], dir[2]);
+		char optV[5] = PROT_OPT(0, 1, dir[0], dir[1], dir[2]);
 		websiteClient.Send(optV,5);
 
-		char *preWeb = (char*)calloc(2,sizeof(char));
+		char *preWeb = (char*)calloc(sizeof(char),2);
 		websiteClient.Read(preWeb,2);
-		
+		std::cout << "PreWeb:" << preWeb[0] << " " << preWeb[1] << std::endl;
+
 		if(preWeb[0]==0)
 		{
 			char complete = 0;
 			websiteClient.Send(&complete, 1);
+			char opSize = 0;
+			websiteClient.Read(&opSize,1);
+			char *opTypes = (char*)calloc(sizeof(char), (int)opSize);
+			websiteClient.Read(opTypes, (int)opSize);
+			std::cout << opTypes << std::endl;
+			for(int i = 0;i<strlen(opTypes);i++)
+			{
+				if(opTypes[i]=='i') sendInt(&websiteClient, 0);
+				else if(opTypes[i]=='c') sendChar(&websiteClient, '0');
+			}
+			std::cout << strlen(opTypes) << " ops sended." << std::endl;
 
 			char *web = (char*)calloc(pow(2,(int)preWeb[1]), sizeof(char));
 			websiteClient.Read(web, pow(2,(int)preWeb[1]));
-			std::cout << "domain<" << dns << ">" << std::endl;
+			std::cout << "domain<" << servername << ">" << std::endl;
 			std::cout << web << std::endl;
-		}
+		} else std::cout << "VeebreWeb Error:" << (int)preWeb[0] << std::endl;
 	}
 	return 0;
 }
--- webserver.cpp
+++ webserver.cpp
@@ -8,12 +8,50 @@
 
 #include "basic_socket.h"
 
-char version[2] = {0,0};
+char version[2] = {0,1};
 
-std::string siteByte, errorByte = "Version not matching with server!";
+struct PageInfo
+{
+	char size; // Data will be pow(2,size) bytes
+	std::string path,opTypes;
+	std::string pageByte;
 
-#define SITE_BYTE 10 //Site scale
-#define ERROR_BYTE 6
+	char getSize() { for(int i = 0;i<255;i++) if(pow(2,i)>pageByte.size()) return i; return '?';}
+	PageInfo(std::string path, std::string opTypes = "")
+	{
+
+		std::ifstream PageText; PageText.open(path);
+		std::stringstream PageStream; PageStream << PageText.rdbuf(); PageText.close();
+		pageByte = PageStream.str();
+		size = getSize();
+		this->opTypes = opTypes;
+
+		std::cout << path << " readed for Ops:" << opTypes <<  ":" << std::endl << pageByte << std::endl;
+	}
+};
+
+std::vector<PageInfo> Pages;
+#define MAINPAGE 0
+#define ERRORPAGE 1
+
+int getPage(int p1, int p2, int p3)
+{
+	return MAINPAGE;
+}
+
+int readInt(int socket)
+{
+	char* integerVal = (char*)calloc(sizeof(char), 4);
+	ISocketSv::Read(socket, integerVal, 4);
+	return *(int*)(integerVal);
+}
+
+char readChar(int socket)
+{
+	char charVal = 0;
+	ISocketSv::Read(socket, &charVal, 1);
+	return charVal;
+}
 
 bool handler(int socket)
 {
@@ -21,33 +59,53 @@ bool handler(int socket)
 
 	char optV[5];
 	if(!ISocketSv::Read(socket, optV, 5)) return false; // Getting optV data
-	std::cout << "Version:" << (int)optV[0] << ":" << (int)optV[1] << std::endl;
+	std::cout << "Version:" << (int)optV[0] << ":" << (int)optV[1] 
+		<< "(should be " << (int)version[0] << ":" << (int)version[1] << ")" << std::endl;
+
 	std::cout << "Directory:" << (int)optV[2] << ":" << (int)optV[3] << ":" << (int)optV[4] << std::endl;
+	
+	char preweb[2] = {0,0}; // Sending preweb data
+	
+	int page = getPage((int)optV[2], (int)optV[3],(int)optV[4]); preweb[1] = Pages[page].size;
+	if(optV[0] != version[0] || optV[1] != version[1]) { preweb[0] = page = ERRORPAGE; }
 
-	bool versionGood = false;
-	if(optV[0] == version[0] && optV[1] == version[1]) versionGood = true;
-
-	char preweb[2] = {(char)0,versionGood == true? (char)SITE_BYTE : (char)ERROR_BYTE}; // Sending preweb data
+	std::cout << "Sending preweb:" << (int)preweb[0] << ":" << (int)preweb[1] << std::endl;
 	if(!ISocketSv::Send(socket, preweb, 2)) return false; 
+	if(preweb[0] != 0) return false; // Stop Veepre if a error corrupted.
 
 	char complete = 0;
 	if(!ISocketSv::Read(socket, &complete, 1)) return false; // Reading complete data
+	
+	char opTypeSize = Pages[page].opTypes.size();
+	std::cout << "Sending Op Size:" << (int)opTypeSize 
+		<< " and type:"<<Pages[page].opTypes<<"."<< std::endl;
+	if(!ISocketSv::Send(socket, &opTypeSize,1)) return false; // Sending OpSize
+	if((int)opTypeSize > 0) 
+	{
+		if(!ISocketSv::Send(socket, (char*)Pages[page].opTypes.c_str(), (int)opTypeSize )) return false; // Sending OpTypes
+		std::cout << "Readed Opts:" ;
+		for(int i = 0 ;i<(int)opTypeSize;i++)
+		{
+			if(Pages[page].opTypes[i]=='c') std::cout << readChar(socket);//reading char
+			else if(Pages[page].opTypes[i]=='i') std::cout << readInt(socket); //reading integer
+		}
+	} std::cout << std::endl << (int)opTypeSize << " op reading ended." << std::endl;
 
 	if(complete == (char)0) // If complete equals to 0, that is successfull
 	{
-		if(versionGood) ISocketSv::Send(socket, (char*)siteByte.c_str(), pow(2,SITE_BYTE)); // sending site
-		else ISocketSv::Send(socket, (char*)errorByte.c_str(), errorByte.length()); // sending error
-	} 
+		ISocketSv::Send(socket, (char*)Pages[page].pageByte.c_str(),pow(2,Pages[page].size));
+		std::cout << "Sending PageId:" << page << "." << std::endl;
+	}
+	std::cout << "Communication end for socket " << socket << "." << std::endl << std::endl;
 
 	return false;
 }
 
 int main(int argc, char *argv[])
 {
-	siteByte = "Welcome to AmmarOzkan Blog Site!";
-	std::ifstream siteTxt; siteTxt.open("index");
-	std::stringstream siteData; siteData << siteTxt.rdbuf();
-	siteByte = siteData.str();
+	Pages.push_back(PageInfo("index", "ccccccc")); // MainPage
+	Pages.push_back(PageInfo("error", "")); // ErrorPage
+	Pages.push_back(PageInfo("info", "")); // 2 
 
 	ISocketSv server(SOCK_STREAM, 4545, 5);
 	server.Begin(handler);
